b2351618d2e534f81847a1c3ce96d2c0f8787b2d
Christopher Adams <christopher@fabricatorz.com>
2016-08-22 16:06:24 +0800

    Add hex description and package

--- mix.exs
+++ mix.exs
@@ -7,6 +7,9 @@ defmodule PhoenixRest.Mixfile do
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     docs: [extras: ["README.md"]],
+     description: description(),
+     package: package(),
      deps: deps()]
   end
 
@@ -18,4 +21,20 @@ defmodule PhoenixRest.Mixfile do
     [{:phoenix, "~> 1.1.0"},
      {:plug_rest, "~> 0.7.0"}]
   end
+
+  defp description do
+    """
+    Resource routing and REST behaviour for Phoenix applications
+    """
+  end
+
+  defp package do
+    [
+     name: :phoenix_rest,
+     maintainers: ["Christopher Adams"],
+     licenses: ["MIT"],
+     links: %{github: "https://github.com/christopheradams/phoenix_rest"}
+    ]
+  end
+
 end
