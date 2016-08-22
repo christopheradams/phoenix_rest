f8e8e054d8b6c7dd5a3bc99ae4a8935e6882d348
Christopher Adams <christopher@fabricatorz.com>
2016-08-22 16:06:13 +0800

    Add module docs

--- lib/phoenix_rest.ex
+++ lib/phoenix_rest.ex
@@ -1,2 +1,16 @@
 defmodule PhoenixRest do
+  @moduledoc """
+  This is the documentation for the PhoenixRest project.
+
+  PhoenixRest makes the PlugRest library available for direct use in a Pheonix
+  router, by adding a `resource` macro which dispatches requests to handler
+  modules implementing REST semantics via a series of optional callbacks.
+
+  For more information see:
+
+    * [PlugRest](https://hexdocs.pm/plug_rest) - an Elixir port of Cowboy's
+      REST sub-protocol for Plug applications
+    * [Phoenix](https://hexdocs.pm/phoenix) - a productive web framework that
+      does not compromise speed and maintainability.
+  """
 end
--- lib/phoenix_rest/router.ex
+++ lib/phoenix_rest/router.ex
@@ -1,4 +1,61 @@
 defmodule PhoenixRest.Router do
+  @moduledoc ~S"""
+  A DSL to supplement Phoenix Router with a resource-oriented routing algorithm.
+
+  It provides a macro to generate routes that dispatch to specific
+  resource handlers.
+
+  Edit `web/web.ex` and add the router:
+
+      def router do
+        quote do
+          use Phoenix.Router
+          use PhoenixRest.Router
+        end
+      end
+
+  Then use the `resource` macro in your router to match a path with a
+  resource handler:
+
+      defmodule MyApp.Router do
+        use HelloPhoenix.Web, :router
+
+        resource "/pages/:page", PageResource
+      end
+
+  The `resource/2` macro accepts a request of format `"/pages/VALUE"` and
+  dispatches it to the `PageResource` module, which must adopt the
+  `PlugRest.Resource` behaviour by implementing one or more of the callbacks
+  which describe the resource.
+
+  See the `PlugRest` documentation for more details about defining a Resource.
+
+  ## Options
+
+  The macro accepts an optional initial state for the resource. For example:
+
+      resource "/pages/:page", PageResource, state: %{option: true}
+
+  ## Routes
+
+      resource "/hello", HelloResource
+
+  The example above will route any requests for "/hello" to the
+  `HelloResource` module.
+
+  A route can also specify parameters which will be available to the
+  resource:
+
+      resource "/hello/:name", HelloResource
+
+  The value of the dynamic path segment can be read inside the
+  `HelloResource` module:
+
+      def to_html(%{params: params} = conn, state) do
+        %{"name" => name} = params
+        {"Hello #{name}!", conn, state}
+      end
+  """
 
   @known_methods ["GET", "HEAD", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"]
 
