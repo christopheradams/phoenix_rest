799a8ccfc39d3f4be87b00245ca4ecb6678368e2
Christopher Adams <christopher@fabricatorz.com>
2016-09-13 22:51:43 +0800

    Test resource options passed to Phoenix match

--- test/phoenix_rest/router_test.exs
+++ test/phoenix_rest/router_test.exs
@@ -27,6 +27,14 @@ defmodule PhoenixRest.RouterTest do
     end
   end
 
+  defmodule OptionsResource do
+    use PhoenixRest.Resource
+
+    def to_html(conn, state) do
+      {conn.assigns.msg, conn, state}
+    end
+  end
+
   defmodule OtherPlug do
     def init(options) do
       options
@@ -43,6 +51,8 @@ defmodule PhoenixRest.RouterTest do
     resource "/hello", HelloResource
     resource "/hello/:message", MessageResource
     resource "/greeting/:message", MessageResource, "Welcome"
+
+    resource "/options", OptionsResource, [], assigns: %{msg: "Hello world"}
 
     resource "/plug", OtherPlug, "Hello world"
   end
@@ -112,5 +122,13 @@ defmodule PhoenixRest.RouterTest do
     conn = conn(:get, "/greeting/everyone")
     conn = RestRouter.call(conn, [])
     assert conn.resp_body == "Welcome everyone!"
+  end
+
+  test "GET /options" do
+    conn = conn(:get, "/options")
+    conn = RestRouter.call(conn, [])
+
+    assert conn.status == 200
+    assert conn.resp_body == "Hello world"
   end
 end
