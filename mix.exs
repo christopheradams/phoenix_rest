74ffa46ebf26ee6d82456071bd796cc6d90d2db6
Christopher Adams <christopher@fabricatorz.com>
2020-01-26 21:21:26 +0800

    Update Elixir version to test

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule PhoenixRest.Mixfile do
     [
       app: :phoenix_rest,
       version: @version,
-      elixir: "~> 1.3",
+      elixir: "~> 1.5",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       dialyzer: [plt_add_apps: [:mix]],
@@ -25,7 +25,7 @@ defmodule PhoenixRest.Mixfile do
   defp deps do
     [
       {:phoenix, "~> 1.3.0"},
-      {:plug_rest, "~> 0.13"},
+      {:plug_rest, path: "../plug_rest"},
       {:dialyxir, "~> 0.5.0", only: [:dev]},
       {:ex_doc, ">= 0.0.0", only: :dev}
     ]
