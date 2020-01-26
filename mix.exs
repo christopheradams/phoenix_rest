feab6b70690e0222e4f704ce3d347184ecf9eae9
Christopher Adams <christopher@fabricatorz.com>
2020-01-26 21:30:38 +0800

    Update to PlugRest 0.14

--- mix.exs
+++ mix.exs
@@ -27,7 +27,7 @@ defmodule PhoenixRest.Mixfile do
   defp deps do
     [
       {:phoenix, "~> 1.3.0"},
-      {:plug_rest, "~> 0.13"},
+      {:plug_rest, "~> 0.14"},
       {:dialyxir, "~> 0.5.0", only: [:dev]},
       {:ex_doc, ">= 0.0.0", only: :dev}
     ]
