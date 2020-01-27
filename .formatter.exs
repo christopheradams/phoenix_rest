c6ed8cf5a0ef06271b184c392fbf3c30ddbeef89
Christopher Adams <christopher@fabricatorz.com>
2020-01-27 10:36:39 +0800

    Import Phoenix into formatter

--- .formatter.exs
+++ .formatter.exs
@@ -6,6 +6,7 @@ export_locals_without_parens = [
 
 [
   inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"],
+  import_deps: [:phoenix],
   locals_without_parens: export_locals_without_parens,
   export: [locals_without_parens: export_locals_without_parens]
 ]
